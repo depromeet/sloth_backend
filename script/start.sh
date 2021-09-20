93d56cc7405e415816d03c40fdd93bee566b68f9
yulhee741 <dbfgml980413@gmail.com>
2021-09-20 23:55:23 +0900

    refactor start.sh 수정

--- script/start.sh
+++ script/start.sh
@@ -12,10 +12,10 @@ echo "현재 쉬고 있는 port : ${IDLE_PORT}"
 if [ ${IDLE_PORT} -eq 8081 ]
 then
         echo "8081 포트 애플리케이션 실행"
-        sudo docker run -d --name spring-template-real1 --rm -p 8081:8081 -e "SPRING_PROFILES_ACTIVE=real1" goohoon/springboot-template
+        sudo docker run -d --name sloth-real1 --rm -p 8081:8081 -e "SPRING_PROFILES_ACTIVE=real1" dbfgml741/sloth
 else
         echo "8082 포트 애플리케이션 실행"
-        sudo docker run -d --name spring-template-real2 --rm -p 8082:8082 -e "SPRING_PROFILES_ACTIVE=real2" goohoon/springboot-template
+        sudo docker run -d --name sloth-real2 --rm -p 8082:8082 -e "SPRING_PROFILES_ACTIVE=real2" dbfgml741/sloth
 fi
 
 sleep 5
