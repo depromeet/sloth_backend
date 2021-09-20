bb5219d871860bb7108eea5265a166ea6e568f96
byeungoo <dksekfldks65@naver.com>
2021-09-21 00:22:12 +0900

    bug : Profile 체크 api 경로 수정

--- script/profile.sh
+++ script/profile.sh
@@ -5,13 +5,13 @@
 # 쉬고 있는 profile 찾기: real1이 사용중이면 real2가 쉬고 있고, 반대면 real1이 쉬고 있음
 function find_idle_profile()
 {
-    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/profile)
+    RESPONSE_CODE=$(curl -s -o /dev/null -w "%{http_code}" http://localhost/api/profile)
 
     if [ ${RESPONSE_CODE} -ge 400 ] # 400 보다 크면 (즉, 40x/50x 에러 모두 포함)
     then
         CURRENT_PROFILE=real2
     else
-        CURRENT_PROFILE=$(curl -s http://localhost/profile)
+        CURRENT_PROFILE=$(curl -s http://localhost/api/profile)
     fi
 
     if [ ${CURRENT_PROFILE} == real1 ]
