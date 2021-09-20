5746a706b15c2f2f2e55e99c3fe8f18b62cca1cc
byeungoo <dksekfldks65@naver.com>
2021-09-21 00:16:31 +0900

    test : 무중단 배포 테스트

--- script/health.sh
+++ script/health.sh
@@ -23,7 +23,7 @@ do
       switch_proxy $IDLE_PORT
       break
   else
-      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다."
+      echo "> Health check의 응답을 알 수 없거나 혹은 실행 상태가 아닙니다!"
       echo "> Health check: ${RESPONSE}"
   fi
 
