f8e1ffbbd16727c88a8ce0613f0bc8dd1dfff5af
SDB016 <dongbin4013@naver.com>
2021-10-13 03:14:28 +0900

    bug: swagger 에러

--- src/main/java/com/sloth/config/WebConfig.java
+++ src/main/java/com/sloth/config/WebConfig.java
@@ -13,6 +13,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -56,7 +57,8 @@ public class WebConfig implements WebMvcConfigurer {
                         HttpMethod.POST.name(),
                         HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name()
-                );
+                )
+                .allowCredentials(false).maxAge(3600);
     }
 
 }
