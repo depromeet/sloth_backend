5e853ead43e59d26523113fcee1868fc7d89461c
yulhee741 <dbfgml980413@gmail.com>
2021-10-13 21:36:50 +0900

    debug: https로 변환 후 swagger 에러 발생 해결

--- src/main/java/com/sloth/config/SwaggerConfig.java
+++ src/main/java/com/sloth/config/SwaggerConfig.java
@@ -5,29 +5,38 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+
 @Configuration
+@EnableOpenApi
 public class SwaggerConfig {
-
-    private ApiInfo apiInfo() {
-
-        return new ApiInfoBuilder()
-                .title("sloth api info")
-                .description("sloth API")
-                .build();
-    }
 
     @Bean
     public Docket commonApi() {
+        Server serverLocal = new Server("local", "http://localhost:8080", "for local usages", Collections.emptyList(), Collections.emptyList());
+        Server testServer = new Server("test", "https://slothbackend.hopto.org", "for testing", Collections.emptyList(), Collections.emptyList());
         return new Docket(DocumentationType.OAS_30)
+                .servers(serverLocal, testServer)
                 .groupName("sloth")
                 .apiInfo(this.apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.sloth.api"))
                 .paths(PathSelectors.ant("/api/**"))
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("sloth api info")
+                .description("sloth API")
+                .version("1.0.0")
                 .build();
     }
 
