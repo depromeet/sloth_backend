6b11318efe8277289f03e45aeb51e23ca644c7e8
byeungoo <dksekfldks65@naver.com>
2021-10-15 12:22:46 +0900

    bug : SiteRepository JpaRepository extends 시 제네릭 파라미터 순서 변경

--- src/main/java/com/sloth/domain/site/repository/SiteRepository.java
+++ src/main/java/com/sloth/domain/site/repository/SiteRepository.java
@@ -5,5 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface SiteRepository extends JpaRepository<Long, Site> {
+public interface SiteRepository extends JpaRepository<Site, Long> {
 }
