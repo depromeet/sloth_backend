5b57a7f82a6a37dbf3ad17e9ed10a98ac511a333
byeungoo <dksekfldks65@naver.com>
2021-10-20 00:59:51 +0900

    refactor : 카테고리 insert 쿼리 추가

--- script/db/category.sql
+++ script/db/category.sql
@@ -1,3 +1,48 @@
-select  *
-from    category
+insert into category
+values(nextval('hibernate_sequence'), 0, '개발', CURRVAL('hibernate_sequence'), '개발')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '디자인', CURRVAL('hibernate_sequence'), '디자인')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '데이터', CURRVAL('hibernate_sequence'), '데이터')
+;
+
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '외국어', CURRVAL('hibernate_sequence'), '외국어')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '금융 재태크', CURRVAL('hibernate_sequence'), '운동')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '직무교육', CURRVAL('hibernate_sequence'), '직무교육')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '드로잉', CURRVAL('hibernate_sequence'), '드로잉')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '공예', CURRVAL('hibernate_sequence'), '공예')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '음악', CURRVAL('hibernate_sequence'), '음악')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '글쓰기', CURRVAL('hibernate_sequence'), '글쓰기')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '컨퍼런스 세미나', CURRVAL('hibernate_sequence'), '컨퍼런스 세미나')
+;
+
+insert into category
+values(nextval('hibernate_sequence'), 0, '기타', CURRVAL('hibernate_sequence'), '기타')
 ;
