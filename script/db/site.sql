448e32f0c70fa60aa9125b412448f620f15580a8
byeungoo <dksekfldks65@naver.com>
2021-10-19 23:29:44 +0900

    refactor : 스크립트 및 강의,사이트 넣는 쿼리 저장

---
+++ script/db/site.sql
@@ -0,0 +1,23 @@
+insert into site
+values(nextval('hibernate_sequence'), '패스트캠퍼스')
+;
+
+insert into site
+values(nextval('hibernate_sequence'), '인프런')
+;
+
+insert into site
+values(nextval('hibernate_sequence'), '클래스 101')
+;
+
+insert into site
+values(nextval('hibernate_sequence'), '탈잉')
+;
+
+insert into site
+values(nextval('hibernate_sequence'), '해커스')
+;
+
+insert into site
+values(nextval('hibernate_sequence'), '파고다')
+;
