fa062b97b62b4572ecfd5082402dfc109b68ad50
byeungoo <dksekfldks65@naver.com>
2021-10-19 22:24:06 +0900

    bug #60 : 테스트코드 멈춰있는 문제 수정

--- src/test/java/com/sloth/api/BaseApiController.java
+++ src/test/java/com/sloth/api/BaseApiController.java
@@ -7,12 +7,14 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.Date;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 public class BaseApiController {
 
     @Autowired
