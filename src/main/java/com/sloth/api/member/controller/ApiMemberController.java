fb906ca1655696ae24873018409251ba78fbaf50
SDB016 <dongbin4013@naver.com>
2021-10-21 01:08:06 +0900

    feat: 멤버 상세 조회 api 변경

--- src/main/java/com/sloth/api/member/controller/ApiMemberController.java
+++ src/main/java/com/sloth/api/member/controller/ApiMemberController.java
@@ -1,38 +1,47 @@
 package com.sloth.api.member.controller;
 
 
+import com.sloth.api.member.dto.MemberInfoDto;
 import com.sloth.api.member.dto.MemberUpdateDto;
-import com.sloth.api.member.dto.MemberDto;
 import com.sloth.api.member.service.ApiMemberService;
 import io.swagger.annotations.ApiImplicitParam;
 import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
+import org.apache.http.HttpHeaders;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("/api/members")
+@RequestMapping("/api/member")
 public class ApiMemberController {
 
     private final ApiMemberService memberService;
 
+
     @Operation(summary = "회원 정보 조회 API", description = "회원 정보 조회 API")
-    @GetMapping("/{memberId}")
-    public MemberDto findById (@PathVariable Long memberId) {
-        return memberService.findById(memberId);
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", defaultValue ="jwt access token", dataType = "string", value = "jwt access token", required = true, paramType = "header")
+    })
+    @GetMapping("")
+    public ResponseEntity<MemberInfoDto> getMemberInfo(HttpServletRequest httpServletRequest) {
+        String token = httpServletRequest.getHeader(HttpHeaders.AUTHORIZATION);
+        MemberInfoDto memberInfo = memberService.getMemberInfo(token);
+        return ResponseEntity.ok(memberInfo);
     }
+
 
     @Operation(summary = "회원 정보 수정 API", description = "회원 정보 수정 API")
     @ApiImplicitParams({
             @ApiImplicitParam(name = "Authorization", defaultValue ="jwt access token", dataType = "string", value = "jwt access token", required = true, paramType = "header")
     })
-
     @PatchMapping("/{memberId}")
     public Long update(@PathVariable Long memberId, @RequestBody MemberUpdateDto requestDto) {
         return memberService.update(memberId, requestDto);
     }
-
 
 }
--- src/main/java/com/sloth/api/member/service/ApiMemberService.java
+++ src/main/java/com/sloth/api/member/service/ApiMemberService.java
@@ -1,9 +1,11 @@
 package com.sloth.api.member.service;
 
+import com.sloth.api.member.dto.MemberInfoDto;
 import com.sloth.api.member.dto.MemberUpdateDto;
-import com.sloth.api.member.dto.MemberDto;
+import com.sloth.config.auth.TokenProvider;
 import com.sloth.domain.member.Member;
 import com.sloth.domain.member.repository.MemberRepository;
+import io.jsonwebtoken.Claims;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -14,20 +16,26 @@ import org.springframework.transaction.annotation.Transactional;
 public class ApiMemberService {
 
     private final MemberRepository memberRepository;
+    private final TokenProvider tokenProvider;
 
-    public MemberDto findById (Long id) {
-        Member member = memberRepository.findById(id).orElseThrow( () -> new IllegalArgumentException("해당 정보가 없습니다. id =" + id));
+    public MemberInfoDto findByEmail (String email) {
+        Member member = memberRepository.findByEmail(email).orElseThrow( () -> new IllegalArgumentException("해당 정보가 없습니다. email =" + email));
 
-        return  new MemberDto(member);
+        return new MemberInfoDto(member);
     }
 
     @Transactional
     public Long update(Long id, MemberUpdateDto requestDto) {
-
         Member member = memberRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 정보가 없습니다. id =" + id));
-
         member.update(requestDto.getMemberName());
 
         return id;
+    }
+
+    public MemberInfoDto getMemberInfo(String token) {
+        Claims tokenClaims = tokenProvider.getTokenClaims(token);
+        String email = tokenClaims.getAudience();
+
+        return findByEmail(email);
     }
 }
