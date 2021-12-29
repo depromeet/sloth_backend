a3eea2d0958323a441a54a2e963e67c8e404323f
byeonguhun <dksekfldks65@naver.com>
2021-12-29 12:27:53 +0900

    feat #135 : Oauth Login인 시 신규 회원 여부 Response 값 추가

--- src/main/java/com/sloth/api/login/dto/ResponseJwtTokenDto.java
+++ src/main/java/com/sloth/api/login/dto/ResponseJwtTokenDto.java
@@ -1,6 +1,7 @@
 package com.sloth.api.login.dto;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -9,14 +10,21 @@ import java.util.Date;
 @Getter @Setter
 public class ResponseJwtTokenDto {
 
+    @ApiModelProperty(value = "access token")
     private String accessToken;
 
+    @ApiModelProperty(value = "access token 만료 시간")
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
     private Date accessTokenExpireTime;
 
+    @ApiModelProperty(value = "refresh token")
     private String refreshToken;
 
+    @ApiModelProperty(value = "refresh token 만료 시간")
     @JsonFormat(shape= JsonFormat.Shape.STRING, pattern="yyyy-MM-dd HH:mm:ss", timezone="Asia/Seoul")
     private Date refreshTokenExpireTime;
+
+    @ApiModelProperty(value = "신규 회원 여부")
+    private Boolean isNewMember = false;
 
 }
