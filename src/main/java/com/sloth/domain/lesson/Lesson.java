00f529bb18e4a4e1bb5ba5a825b694294b23b186
byeungoo <dksekfldks65@naver.com>
2022-01-17 23:45:11 +0900

    bug : Lesson 원복

--- src/main/java/com/sloth/domain/lesson/Lesson.java
+++ src/main/java/com/sloth/domain/lesson/Lesson.java
@@ -7,7 +7,6 @@ import com.sloth.domain.member.Member;
 import com.sloth.domain.site.Site;
 import com.sloth.exception.BusinessException;
 import lombok.*;
-import lombok.extern.slf4j.Slf4j;
 import org.hibernate.envers.AuditOverride;
 import org.hibernate.envers.Audited;
 import org.hibernate.envers.RelationTargetAuditMode;
@@ -23,7 +22,6 @@ import java.time.temporal.ChronoUnit;
 @AllArgsConstructor @NoArgsConstructor
 @ToString(exclude = {"category", "member", "site"})
 @EqualsAndHashCode(of = "lessonId", callSuper = false)
-@Slf4j
 @Table(name = "lesson")
 public class Lesson extends BaseEntity  {
 
