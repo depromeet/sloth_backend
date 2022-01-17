49d7fc9f5b1acf95775fb54314e5520976991919
byeungoo <dksekfldks65@naver.com>
2022-01-17 23:58:29 +0900

    Revert "bug : 목표 진도율 계산 버그 수정2"
    This reverts commit 4c0071f7

--- src/main/java/com/sloth/domain/lesson/Lesson.java
+++ src/main/java/com/sloth/domain/lesson/Lesson.java
@@ -159,15 +159,16 @@ public class Lesson extends BaseEntity  {
             return 0;
         }
 
-        int goalProgress = (int) Math.floor( (double) getGoalNumber(now) / (double) getTotalNumber() * 100);
-        if (goalProgress > 100) {
-            return 100;
-        }
+       int goalProgress = (int) Math.floor( (double) getGoalNumber(now) / (double) getTotalNumber() * 100);
+        goalProgress = goalProgress <= 100 ? goalProgress : 100;
+
+        log.info("goalProgress : {}", goalProgress);
 
         return goalProgress;
     }
 
     public int getWastePrice(LocalDate now) {
+        int x = getGoalProgressRate(now);
         int wastePrice = (int) (price * ((double) (getGoalProgressRate(now) - getCurrentProgressRate()) / (double) 100));
         return wastePrice >= 0 ? wastePrice : 0;
     }
