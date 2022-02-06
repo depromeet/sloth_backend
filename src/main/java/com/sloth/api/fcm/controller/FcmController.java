c5e2c8535af52176bedba757a056db882ce9d328
yulhee741 <dbfgml980413@gmail.com>
2022-02-06 23:48:58 +0900

    feat: S3에서 key file 가져오기

--- src/main/java/com/sloth/api/fcm/controller/FcmController.java
+++ src/main/java/com/sloth/api/fcm/controller/FcmController.java
@@ -1,6 +1,8 @@
 package com.sloth.api.fcm.controller;
 import com.sloth.api.fcm.dto.FcmDto;
 import com.sloth.api.fcm.service.FirebaseCloudMessageService;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,6 +19,9 @@ public class FcmController {
     private final FirebaseCloudMessageService firebaseCloudMessageService;
 
     @PostMapping("/api/fcm")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "Authorization", defaultValue ="jwt access token", dataType = "string", value = "jwt access token", required = true, paramType = "header")
+    })
     public ResponseEntity pushMessage(@RequestBody FcmDto.Request requestDTO) throws IOException {
         System.out.println(requestDTO.getTargetToken() + " "
                 +requestDTO.getTitle() + " " + requestDTO.getBody());
