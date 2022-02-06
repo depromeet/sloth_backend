75e7fffa185ff8c826559fa314939da4ae0ef6e9
byeungoo <dksekfldks65@naver.com>
2022-02-07 00:34:01 +0900

    feat : aws s3 키 암호화 수정

--- src/main/java/com/sloth/global/config/AwsS3Config.java
+++ src/main/java/com/sloth/global/config/AwsS3Config.java
@@ -1,8 +1,7 @@
-package com.sloth.config;
+package com.sloth.global.config;
 
 import com.amazonaws.auth.AWSStaticCredentialsProvider;
 import com.amazonaws.auth.BasicAWSCredentials;
-import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.AmazonS3Client;
 import com.amazonaws.services.s3.AmazonS3ClientBuilder;
 import org.springframework.beans.factory.annotation.Value;
