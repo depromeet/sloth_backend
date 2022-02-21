d7c987ba243eb8dbff8de328277f291d5e10c818
byeungoo <dksekfldks65@naver.com>
2022-02-21 23:46:31 +0900

    refactor #172 : ErrorResponse -> ErrorResponseDto로 클래스명 수정

--- src/main/java/com/sloth/global/exception/handler/GlobalExceptionHandler.java
+++ src/main/java/com/sloth/global/exception/handler/GlobalExceptionHandler.java
@@ -30,7 +30,7 @@ public class GlobalExceptionHandler {
      * 주로 @RequestBody, @RequestPart 어노테이션에서 발생
      */
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    protected ResponseEntity<ErrorMessage> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleMethodArgumentNotValidException(MethodArgumentNotValidException e, HttpServletRequest request) {
         log.error("handleMethodArgumentNotValidException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST, request.getRequestURI());
     }
@@ -39,7 +39,7 @@ public class GlobalExceptionHandler {
      * @ModelAttribut 으로 binding error 발생시 BindException 발생한다.
      */
     @ExceptionHandler(BindException.class)
-    protected ResponseEntity<ErrorMessage> handleBindException(BindException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleBindException(BindException e, HttpServletRequest request) {
         log.error("handleBindException", e);
 
         BindingResult bindingResult = e.getBindingResult();
@@ -59,7 +59,7 @@ public class GlobalExceptionHandler {
      * 주로 @RequestParam enum으로 binding 못했을 경우 발생
      */
     @ExceptionHandler(MethodArgumentTypeMismatchException.class)
-    protected ResponseEntity<ErrorMessage> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e, HttpServletRequest request) {
         log.error("handleMethodArgumentTypeMismatchException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST, request.getRequestURI());
     }
@@ -68,7 +68,7 @@ public class GlobalExceptionHandler {
      * 지원하지 않은 HTTP method 호출 할 경우 발생
      */
     @ExceptionHandler(HttpRequestMethodNotSupportedException.class)
-    protected ResponseEntity<ErrorMessage> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleHttpRequestMethodNotSupportedException(HttpRequestMethodNotSupportedException e, HttpServletRequest request) {
         log.error("handleHttpRequestMethodNotSupportedException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED, request.getRequestURI());
     }
@@ -77,14 +77,14 @@ public class GlobalExceptionHandler {
      * Authentication 객체가 필요한 권한을 보유하지 않은 경우 발생합
      */
     @ExceptionHandler(AccessDeniedException.class)
-    protected ResponseEntity<ErrorMessage> handleAccessDeniedException(AccessDeniedException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleAccessDeniedException(AccessDeniedException e, HttpServletRequest request) {
         log.error("handleAccessDeniedException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI());
     }
 
     @ExceptionHandler(value = { BusinessException.class })
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    protected ResponseEntity<ErrorMessage> handleConflict(BusinessException e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleConflict(BusinessException e, HttpServletRequest request) {
         log.error("GlobalException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, request.getRequestURI());
     }
@@ -116,46 +116,46 @@ public class GlobalExceptionHandler {
 
     @ExceptionHandler(MemberTokenNotFoundException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<ErrorMessage> memberNotFoundExceptionHandle(MemberTokenNotFoundException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> memberNotFoundExceptionHandle(MemberTokenNotFoundException e, HttpServletRequest request) {
         log.error("MemberTokenNotFoundException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI());
     }
 
     @ExceptionHandler(FeignClientException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<ErrorMessage> memberNotFoundExceptionHandle(FeignClientException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> memberNotFoundExceptionHandle(FeignClientException e, HttpServletRequest request) {
         log.error("FeignClientException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI());
     }
 
     @ExceptionHandler(InvalidParameterException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ResponseEntity<ErrorMessage> invalidParameterExceptionHandle(InvalidParameterException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> invalidParameterExceptionHandle(InvalidParameterException e, HttpServletRequest request) {
         log.error("InvalidParameterException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.BAD_REQUEST, request.getRequestURI());
     }
 
     @ExceptionHandler(NeedEmailConfirmException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<ErrorMessage> needEmailConfirmExceptionHandle(NeedEmailConfirmException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> needEmailConfirmExceptionHandle(NeedEmailConfirmException e, HttpServletRequest request) {
         log.error("NeedEmailConfirmException", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI());
     }
 
     @ExceptionHandler(ForbiddenException.class)
     @ResponseStatus(HttpStatus.FORBIDDEN)
-    public ResponseEntity<ErrorMessage> forbiddenExceptionHandle(ForbiddenException e, HttpServletRequest request) {
+    public ResponseEntity<ErrorResponseDto> forbiddenExceptionHandle(ForbiddenException e, HttpServletRequest request) {
         return exceptionResponseEntity(e.getMessage(), HttpStatus.FORBIDDEN, request.getRequestURI());
     }
 
     @ExceptionHandler(Exception.class)
-    protected ResponseEntity<ErrorMessage> handleException(Exception e, HttpServletRequest request) {
+    protected ResponseEntity<ErrorResponseDto> handleException(Exception e, HttpServletRequest request) {
         log.error("Exception", e);
         return exceptionResponseEntity(e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR, request.getRequestURI());
     }
 
-    private ResponseEntity<ErrorMessage> exceptionResponseEntity(String message, HttpStatus status, String requestURI) {
-        ErrorMessage em = ErrorMessage.builder()
+    private ResponseEntity<ErrorResponseDto> exceptionResponseEntity(String message, HttpStatus status, String requestURI) {
+        ErrorResponseDto em = ErrorResponseDto.builder()
                 .errorMessage(message)
                 .code(status.value())
                 .referedUrl(requestURI)
