819e66227e4d9a9cdd8587b27d6d85a6a20a6a05
dongbin Shin <59786670+SDB016@users.noreply.github.com>
2022-02-27 23:09:30 +0900

    feat: 로그아웃 컨트롤러 테스트

--- src/test/java/com/sloth/api/logout/controller/LogoutControllerTest.java
+++ src/test/java/com/sloth/api/logout/controller/LogoutControllerTest.java
@@ -6,6 +6,7 @@ import com.sloth.global.config.auth.TokenProvider;
 import com.sloth.test.base.NewBaseApiController;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.mockito.BDDMockito;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -17,6 +18,9 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import java.time.LocalDateTime;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.BDDMockito.*;
 import static org.mockito.Mockito.doNothing;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -40,19 +44,19 @@ class LogoutControllerTest extends NewBaseApiController {
         mockMvc = MockMvcBuilders.standaloneSetup(logoutController).build();
     }
 
-    //@Test
-    @DisplayName("로그아웃 테스트 - localDateTime.now() 때문에 테스트 불가능 ㅠ")
+    @Test
+    @DisplayName("로그아웃 테스트")
     void logoutTest() throws Exception {
 
         // given
         String email = "test@test.com";
-        doNothing().when(logoutService).logout(email, LocalDateTime.now());
-
-        BDDMockito.when(tokenProvider.getEmail("accessToken")).thenReturn(email);
+        final String accessToken = "accessToken";
+        doNothing().when(logoutService).logout(eq(email), any());
+        when(tokenProvider.getEmail(accessToken)).thenReturn(email);
 
         // when
         ResultActions result = mockMvc.perform(post("/api/logout")
-                .header(HttpHeaders.AUTHORIZATION, "accessToken")
+                .header(HttpHeaders.AUTHORIZATION, accessToken)
                 .accept(MediaType.TEXT_PLAIN_VALUE)
         );
 
