22d237efd6e574d0d7e22edb546bc951ed9a2148
byeungoo <dksekfldks65@naver.com>
2023-01-06 16:59:33 +0900

    feature : 개발 db 주소 수정 및 stop.sh 수정

--- script/sh/stop.sh
+++ script/sh/stop.sh
@@ -6,31 +6,80 @@ ABSDIR=$(dirname $ABSPATH)
 source ${ABSDIR}/profile.sh
 
 IDLE_PORT=$(find_idle_port)
-echo "현재 쉬고 있는 port : ${IDLE_PORT}"
+echo "idle port : ${IDLE_PORT}"
 
 
 RUNNING_RESULT=$(sudo docker ps | grep ${IDLE_PORT})
 
 if [ -z ${RUNNING_RESULT} ]
 then
-      echo "> No applications are currently running"
+    echo "> No applications are currently running"
+    $(sudo docker rm -f "sloth-real1")
+    $(sudo docker rm -f "sloth-real2")
+    echo "docker rm -f sloth-real1~2"
+    # sudo docker pull dbfgml741/sloth
+    sudo docker image prune -f
+    echo "stop.sh complete"
 else
 
   if [ ${IDLE_PORT} -eq 8081 ]
   then
         echo "8081 application stop"
-        $(sudo docker stop "spring-template-real1")
+        $(sudo docker rm -f "sloth-real1")
+        echo "docker rm -f sloth-real1"
   else
         echo "8082 application stop"
-        $(sudo docker stop "spring-template-real2")
+        $(sudo docker rm -f "sloth-real2")
+        echo "docker rm -f sloth-real2"
   fi
 
-  # 도커 이미지 pull
-  sudo docker pull dbfgml741/sloth
-
-  #미사용 도커 이미지 삭제
+  # sudo docker pull dbfgml741/sloth
   sudo docker image prune -f
 
   sleep 5
+  echo "stop.sh complete"
 
+fi
+
+
+#!/usr/bin/env bash
+
+ABSPATH=$(readlink -f $0)
+ABSDIR=$(dirname $ABSPATH)
+
+source ${ABSDIR}/profile.sh
+
+IDLE_PORT=$(find_idle_port)
+echo "idle port : ${IDLE_PORT}"
+
+
+RUNNING_RESULT=$(sudo docker ps | grep ${IDLE_PORT})
+
+if [ -z ${RUNNING_RESULT} ]
+then
+  echo "> No applications are currently running"
+  $(sudo docker rm -f "sloth-real1")
+  $(sudo docker rm -f "sloth-real2")
+  echo "docker rm -f sloth-real1~2"
+  sudo docker pull dbfgml741/sloth
+  sudo docker image prune -f
+  echo "stop.sh complete"
+else
+
+  if [ ${IDLE_PORT} -eq 8081 ]
+  then
+        echo "8081 application stop"
+$(sudo docker rm -f "sloth-real1")
+echo "docker rm -f sloth-real1"
+  else
+        echo "8082 application stop"
+$(sudo docker rm -f "sloth-real2")
+echo "docker rm -f sloth-real2"
+  fi
+
+  sudo docker pull dbfgml741/sloth
+  sudo docker image prune -f
+
+  sleep 5
+  echo "stop.sh complete"
 fi
