32d4c776a77a06f6e9144a358730e371c621e861
byeungoo <dksekfldks65@naver.com>
2023-01-30 22:04:02 +0900

    feature : 쿼리 파라미터 변수 받을 수 있도록 @Setter 어노테이션 추가

--- src/main/java/com/sloth/global/dto/PageDto.java
+++ src/main/java/com/sloth/global/dto/PageDto.java
@@ -1,11 +1,12 @@
 package com.sloth.global.dto;
 
 import lombok.Getter;
+import lombok.Setter;
 
 /**
  * 페이지 조회용 공통 dto
  */
-@Getter
+@Getter @Setter
 public class PageDto {
 
     private int page = 0;
